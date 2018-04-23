214a7923a2bd3cfb01b588dc03e517b0b69aef57
timmytonga <thien997@gmail.com>
2018-04-22 20:30:30 -0700

    update

--- graph.py
+++ graph.py
@@ -6,9 +6,10 @@ class GraphException(Exception):
 
 class Graph:
     ''' Represent a graph object. Undirected by default. Initialize empty graph '''
+
+######################## SETUP AND UTILITIES ############################### 
     def __init__(self, graph=defaultdict(set), weightDict={}, vertices=set(),transitionMatrix=[], isDirected = False ):
-        ''' Input edgeDict --> get vertices from there.
-            Graph representation: {'vertex name': {set of destination} '''
+        ''' Input edgeDict --> get vertices from there.... '''
         self.directed = isDirected  # directed or not
         self.isWeighted = False     # default non weighted
         self.vertices = vertices    # {'A', 'B', 'C', ... }
@@ -20,7 +21,7 @@ class Graph:
             raise GraphException("add_vertex: Vertex " + v + "already in graph")
         self.vertices.add(v)
         self.graph[v] = {}
-
+    
     def add_edge(self, source, dest):
         if source not in self.graph:
             self.add_vertex(source)
@@ -29,7 +30,33 @@ class Graph:
         self.graph[source].add(dest)
         if not self.directed:
             self.graph[dest].add(source)
-        
+
+    def degree(self, vertex):
+        ''' return the degree (in + out deg) of that vertex '''
+        pass
+
+    def in_degree(self, vertex):
+        '''Remove the number of indegree of a vertex
+            only for directed graph... return degree for undirected '''
+        pass
+
+    def out_degree(self, vertex):
+        ''' Remove the number of outdegree edges of a vertex '''
+        pass
+
+    
+
+##################### ALGORITHMS #############################
+''' See NOTES regarding algorithms runnability on directed vs. undirected graphs '''
+    def mst_prim(self, seed):
+        pass
+
+    def mst_boruvka(self):
+        pass
+
+    def mst_kruskal(self):
+        pass
+
     def dfs(self, start):
         ''' return a list of reachable vertices from start '''
         if start not in self.vertices:
@@ -44,7 +71,8 @@ class Graph:
         return visited
                     
     def bfs(self, start):
-        ''' Return all reachable vertex from start -> shortest distance'''
+        ''' Return all reachable vertex from start using shortest path
+            using bfs traversal '''
         reached = { start }
         q = [start]
         while len(q) != 0:
@@ -52,28 +80,27 @@ class Graph:
             for dest in self.graph[v]:
                 if dest not in reached:
                     reached.add(dest)
-                    q.insert(0,dest)
-        return reached
-
-    def all_pairs_shortest(self):
-        pass
+        return reached     
 
     def topological_sort(self):
-        ''' Reverse postorder dfs tree '''
+        ''' Directed graphs only '''
         pass
 
-    def shortest_path_dijkstra(self, start):
+    def dijkstra(self, start):
+        ''' Compute shortest path from a given start vertex
+        NOTE: NO NEGATIVE EDGE WEIGHTS -- use bellman_ford instead ''' 
         pass
 
     def bellman_ford(self, start):
+        ''' Compute shortest path from a given start vertex
+        NO NEGATIVE CYCLES ''' 
         pass
 
-    def mst_prim(self, seed):
+    def astar(self, start, goal, h):
+        ''' compute shortest path from start to goal FASTER than Dijkstra
+        h is a heuristic function that returns the estimated cost to a point from start ''' 
         pass
 
-    def mst_boruvka(self):
-        pass
-
-    def mst_kruskal(self):
-        pass
-    
+    def johnson(self):
+        ''' All pairs shortest paths '''
+        pass 
